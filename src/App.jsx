cef7c1296052a97775a30989543a0fba013a038b
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-10-24 16:40:50 +0100

    removed un-needed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import React, { useContext } from "react";
-import { AuthProvider, AuthContext } from "./context/AuthContext";
-import Home from "./pages/Home";
-import useSpotifyPlayer from "./hooks/useSpotifyPlayer";
-
-function AppContent() {
-  const { token, handleLogin, handleLogout } = useContext(AuthContext);
-
-  // initialize playback SDK when token is available
-  const { player, deviceId, currentTrack, isPlaying } = useSpotifyPlayer(token);
-
-  // pass these values as props to home page
-  return (
-    <Home
-      token={token}
-      handleLogin={handleLogin}
-      handleLogout={handleLogout}
-      player={player}
-      deviceId={deviceId}
-      currentTrack={currentTrack}
-      isPlaying={isPlaying}
-    />
-  );
-}
+import React from 'react';
+import { Home } from './pages/Home';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
-      <AppContent />
+      <Home />
     </AuthProvider>
   );
 }
