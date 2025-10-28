7c40ec8f839eb08766a2b765ff737eae44cb2241
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-10-28 23:58:20 +0000

    added some playlist functions

--- src/services/spotify/spotifyAPI.js
+++ src/services/spotify/spotifyAPI.js
@@ -60,40 +60,10 @@ export const getUserPlaylists = async (limit = 100) => {
   return fetchWithAuth(`/me/playlists?limit=${limit}`);
 };
 
-// bpm, key, energy, danceability, valence
-export const getAudioFeatures = async (token, trackId) => {
-  const res = await fetch(`https://api.spotify.com/v1/audio-features/${trackId}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return res.json();
+export const getPlaylist = async (playlistId) => {
+  return fetchWithAuth(`/playlists/${playlistId}`);
 };
 
-export const getRecommendations = async (token, seedTrackId, filters = {}) => {
-  let url = `https://api.spotify.com/v1/recommendations?seed_tracks=${seedTrackId}&limit=20`;
-  if (filters.key) url += `&target_key=${filters.key}`;
-  if (filters.bpm) url += `&target_tempo=${filters.bpm}`;
-  if (filters.energy) url += `&target_energy=${filters.energy}`;
-  if (filters.danceability) url += `&target_danceability=${filters.danceability}`;
-  if (filters.valence) url += `&target_valence=${filters.valence}`;
-
-  const res = await fetch(url, { headers: { Authorization: `Bearer ${token}` } });
-  const data = await res.json();
-  return data.tracks;
-};
-
-export const playTrack = async (token, deviceId, uri) => {
-  await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
-    method: 'PUT',
-    headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ uris: [uri] }),
-  });
-};
-
-
-export const addToPlaylist = async (token, playlistId, uri) => {
-  await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-    method: 'POST',
-    headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ uris: [uri] }),
-  });
+export const getPlaylistTracks = async (playlistId, limit = 100) => {
+  return fetchWithAuth(`/playlists/${playlistId}/tracks?limit=${limit}`);
 };
