4a891a099dc14a506c32c0905a638ab23016ddee
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-10-29 13:56:37 +0000

    added constants for music analysis

---
+++ src/lib/constants.js
@@ -0,0 +1,28 @@
+export const SPOTIFY_AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+export const SPOTIFY_API_BASE = 'https://api.spotify.com/v1';
+
+export const MUSICAL_KEYS = {
+  0: 'C',
+  1: 'C♯/D♭',
+  2: 'D',
+  3: 'D♯/E♭',
+  4: 'E',
+  5: 'F',
+  6: 'F♯/G♭',
+  7: 'G',
+  8: 'G♯/A♭',
+  9: 'A',
+  10: 'A♯/B♭',
+  11: 'B'
+};
+
+export const MODES = {
+  0: 'Minor',
+  1: 'Major'
+};
+
+// BPM tolerance for similar tracks
+export const BPM_TOLERANCE = 5;
+
+// Key tolerance (semitones)
+export const KEY_TOLERANCE = 2;
