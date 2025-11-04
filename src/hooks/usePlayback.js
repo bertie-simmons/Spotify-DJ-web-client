75c26a3bea5d2dd69d2be8091cddb38a437de21b
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-11-04 18:53:30 +0000

    implemented a functions to interact with queue

--- src/hooks/usePlayback.js
+++ src/hooks/usePlayback.js
@@ -29,7 +29,44 @@ export const usePlayback = () => {
     }
   }, [playMultiple]);
 
-  
+  // Play next track in queue
+  const playNext = useCallback(async () => {
+    if (currentIndex < queue.length - 1) {
+      await next();
+      setCurrentIndex(prev => prev + 1);
+    }
+  }, [next, currentIndex, queue.length]);
+
+  // Play previous track in queue
+  const playPrevious = useCallback(async () => {
+    if (currentIndex > 0) {
+      await previous();
+      setCurrentIndex(prev => prev - 1);
+    }
+  }, [previous, currentIndex]);
+
+  // Add track to queue
+  const addToQueue = useCallback((track) => {
+    setQueue(prev => [...prev, track]);
+  }, []);
+
+  // Clear queue
+  const clearQueue = useCallback(() => {
+    setQueue([]);
+    setCurrentIndex(0);
+  }, []);
+
+  return {
+    queue,
+    currentIndex,
+    playTrack,
+    playTrackList,
+    playNext,
+    playPrevious,
+    togglePlay,
+    addToQueue,
+    clearQueue,
+  };
 };
 
 export default usePlayback;
