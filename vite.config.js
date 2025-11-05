c01c4246fbf05d7ec3192fddcbf67870348644d3
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-11-05 18:15:54 +0000

    fixed a tailwind css bug

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import dns from 'dns';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
   ],
   server: {
     host: '127.0.0.1',
