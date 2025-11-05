b88ef8fcded919e6b31fbfab831416bab6c3c23b
Bertie Simmons Personal <bertiesimmons06@gmail.com>
2025-11-05 15:12:05 +0000

    changed colors to match spotify

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        spotify: {
-          green: '#1DB954',
-          black: '#191414',
-          darkgray: '#121212',
-          gray: '#535353',
-          lightgray: '#B3B3B3',
-          white: '#FFFFFF',
-        }
-      }
+        'spotify-green': '#1DB954',
+        'spotify-black': '#191414',
+        'spotify-dark': '#121212',
+      },
     },
   },
   plugins: [],
